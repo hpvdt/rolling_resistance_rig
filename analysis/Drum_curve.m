50fbdfc0bd3c986c3456a7d313db39593967e5e4
Savo Bajic <bajic.savo@gmail.com>
2023-09-09 20:21:16 -0400

    Cleaned up and commented drum curve script

--- analysis/Drum_curve.m
+++ analysis/Drum_curve.m
@@ -1,3 +1,6 @@
+% This is a script that was prepared to compare what appears to be the
+% wheel's perfomance across different trials to see if the drum's behaviour
+% is consistent between different trials.
 
 Idrum = 6.7575;
 drum_radius = 4.709/(2*pi);
@@ -5,25 +8,23 @@ drum_radius = 4.709/(2*pi);
 %Plots drum power curves
 
 aeroDataDirectory = ['data' filesep 'aero' filesep];
-aeroFileL1 = [aeroDataDirectory 'Michelin 4575r16 - Separate - Left - 60psi.csv'];
-aeroFileR1 = [aeroDataDirectory 'Michelin 4575r16 - Separate - Right - 60psi.csv'];
+drum_L(1) = {[aeroDataDirectory 'Michelin 4575r16 - Separate - Left - 60psi.csv']};
+drum_R(1) = {[aeroDataDirectory 'Michelin 4575r16 - Separate - Right - 60psi.csv']};
 
-aeroFileL2 = [aeroDataDirectory 'Michelin 44-406 - Separate - Left - 80psi.csv'];
-aeroFileR2 = [aeroDataDirectory 'Michelin 44-406 - Separate - Right - 80psi.csv'];
+drum_L(2) = {[aeroDataDirectory 'Michelin 44-406 - Separate - Left - 80psi.csv']};
+drum_R(2) = {[aeroDataDirectory 'Michelin 44-406 - Separate - Right - 80psi.csv']};
 
-aeroFileL3 = [aeroDataDirectory 'Greenspeed - Separate - Left - 80psi.csv'];
-aeroFileR3 = [aeroDataDirectory 'Greenspeed - Separate - Left - 80psi.csv'];
+drum_L(3) = {[aeroDataDirectory 'Greenspeed - Separate - Left - 80psi.csv']};
+drum_R(3) = {[aeroDataDirectory 'Greenspeed - Separate - Left - 80psi.csv']};
 
-% aeroFileL3 = [aeroDataDirectory name_wheel ' - Separate - Left - 50psi.csv'];
-% aeroFileR3 = [aeroDataDirectory name_wheel ' - Separate - Right - 50psi.csv'];
-
-drum_L = [{aeroFileL1}, {aeroFileL2}, {aeroFileL3}];
-drum_R = [{aeroFileR1}, {aeroFileR2}, {aeroFileR3}];
+% drum_L(3) = [aeroDataDirectory name_wheel ' - Separate - Left - 50psi.csv'];
+% drum_R(3) = [aeroDataDirectory name_wheel ' - Separate - Right - 50psi.csv'];
 
 power_drum_left = [];
 speed_low = 0;
 speed_high = 23;
 
+figure(); % Start a new figure for data
 
 for i = 1:3
     speed_tire = [];
@@ -49,21 +50,15 @@ for i = 1:3
         
         totalPower = [totalPower,(power_left + power_right)/2];
     end
-    
-    if i == 1
-        figure()
-    end
+
     plot(speed_tire,totalPower);
-    hold on
-
-    xlabel('Velocity (m/s)', 'fontsize', 12)
-    ylabel('Power (W)', 'fontsize', 12)
-    legend('November 2nd','December 5th','February 19th')
-
     hold on
     
 end
 
-
-
-
+% Dress up the figure at the end
+xlabel('Velocity (m/s)', 'fontsize', 12)
+xlim([0,inf])
+ylabel('Power (W)', 'fontsize', 12)
+ylim([0,inf])
+legend('November 2nd','December 5th','February 19th', 'Location','northwest')
