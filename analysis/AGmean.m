1efa445cd282f15f3e56715e08abe4414cb1403c
Savo Bajic <bajic.savo@gmail.com>
2023-09-09 20:21:59 -0400

    Trying to decipher `AGmean`
    It is only used at one point in inertia calculation, seems to be some mix of geometric and statistical means

--- analysis/AGmean.m
+++ analysis/AGmean.m
@@ -1,13 +1,14 @@
 function [ an ] = AGmean(a0, g0)
-%AGmean Summary of this function goes here
+%AGmean 
 %   Detailed explanation goes here
 tolerance = 1E-10;
 
+% Is this just some numberical approximation of the geometric mean?
 
-an = 0.5*(a0 + g0);
-gn = sqrt(a0 * g0);
+an = 0.5 * (a0 + g0); % Statistical Mean (average)
+gn = sqrt(a0 * g0); % Geometric mean
 
 while abs(an - gn) > tolerance
-    an = 0.5*(an +gn);
-    gn = sqrt(an *gn);
+    an = 0.5 * (an + gn);
+    gn = sqrt(an * gn);
 end
